0e8030861ea50ac9f031d0a740813185e0fcd707
BillyGoat <goatsniffing@gmail.com>
2019-04-23 11:35:22 +0100

    Cleanup and added prettyprint

--- src/index.js
+++ src/index.js
@@ -1,31 +1,60 @@
 const { Command, flags } = require("@oclif/command");
-const { getDomainsByRegistrar } = require("domain-scout");
+const { getDomainsByRegistrant } = require("domain-scout");
 
-class DomainsByRegistrarCommand extends Command {
+// Oclif CLi setup/registration
+class DomainsByRegistrantCommand extends Command {
   async run() {
-    const { flags } = this.parse(DomainsByRegistrarCommand);
-    const registrant = flags.registrant;
-    const domainList = await getDomainsByRegistrar(registrant);
-    const prettyIndent = flags.pretty ? 2 : 0;
-    this.log(JSON.stringify(domainList, null, prettyIndent));
+    const { flags } = this.parse(DomainsByRegistrantCommand);
+    const { registrant, pretty, count, fullOutput } = flags;
+
+    // Get js objects back from domain-scout
+    const domainList = await getDomainsByRegistrant(registrant);
+
+    // If -c provided, just provide total number
+    if (count) return this.log(domainList.length);
+
+    // If -f provided, return whole objects to stdout individually.
+    if (fullOutput) {
+      // If -p provided, prettify by indenting by 2.
+      const prettyIndent = pretty ? 2 : 0;
+      for (let d of domainList) {
+        this.log(JSON.stringify(d, null, prettyIndent));
+      }
+    } else {
+      // If -f output isn't set, just return domains
+      for (let d of domainList) {
+        this.log(d.domain);
+      }
+    }
   }
 }
 
-DomainsByRegistrarCommand.flags = {
+DomainsByRegistrantCommand.flags = {
   // add --version flag to show CLI version
   version: flags.version({ char: "v" }),
   // add --help flag to show CLI version
   help: flags.help({ char: "h" }),
   registrant: flags.string({
     char: "r",
-    desciprtion: "Name of the registrant you wish to check",
+    description: "Name of the registrant you wish to check",
     required: true
   }),
   pretty: flags.boolean({
     char: "p",
-    desciprtion: "Pretty print the output",
+    description: "Pretty print the output when using -f (indent json by 2)",
+    default: false
+  }),
+  count: flags.boolean({
+    char: "c",
+    description:
+      "Just return the total found (Maximum of 500 due to restriction on website)",
+    default: false
+  }),
+  fullOutput: flags.boolean({
+    char: "f",
+    description: "Returns Domain, creation and registrant",
     default: false
   })
 };
 
-module.exports = DomainsByRegistrarCommand;
+module.exports = DomainsByRegistrantCommand;
